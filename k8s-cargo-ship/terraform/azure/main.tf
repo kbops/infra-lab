82dc61a7772a483fdffd70dfd620ddcafc538add
Valerii Holubiuk <valerii.holubiuk@globallogic.com>
2025-06-27 14:58:06 +0300

    eks configuration moved to aws dir

---
+++ k8s-cargo-ship/terraform/azure/main.tf
@@ -0,0 +1,58 @@
+provider "aws" {
+  region = local.region
+}
+
+module "eks" {
+  source                                   = "terraform-aws-modules/eks/aws"
+  cluster_name                             = local.name
+  cluster_version                          = "1.32"
+  cluster_endpoint_public_access           = true
+  cluster_endpoint_public_access_cidrs     = ["195.66.79.218/32"] # Replace with your IP
+  enable_cluster_creator_admin_permissions = true
+  subnet_ids                               = module.vpc.private_subnets
+  vpc_id                                   = module.vpc.vpc_id
+
+  eks_managed_node_groups = {
+    default = {
+      instance_types = ["t3.medium"]
+      capacity_type  = "SPOT" # Added for cost efficiency
+      desired_size   = var.node_desired_size
+      max_size       = 3
+      min_size       = var.node_min_size
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+    Manager     = "Valerii Holubiuk"
+  }
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+  name    = "eks-vpc"
+  cidr    = local.vpc_cidr
+
+  azs             = local.azs
+  private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k)]
+  public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 48)]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+  tags               = local.tags
+}
+
+resource "aws_ecr_repository" "cargo_ship" {
+  name = "cargo-ship"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Name        = "cargo-ship"
+    Environment = "dev"
+  }
+}
---
+++ k8s-cargo-ship/terraform/azure/outputs.tf
@@ -0,0 +1,21 @@
+output "cluster_name" {
+  value = module.eks.cluster_name
+}
+
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "cluster_ca_certificate" {
+  value = module.eks.cluster_certificate_authority_data
+}
+
+output "cargo_ship_ecr_repository_url" {
+  description = "The URL of the ECR repository for k8s-cargo-ship"
+  value       = aws_ecr_repository.cargo_ship.repository_url
+}
+
+output "region" {
+  description = "The AWS region where the EKS cluster is deployed"
+  value       = local.region
+}
